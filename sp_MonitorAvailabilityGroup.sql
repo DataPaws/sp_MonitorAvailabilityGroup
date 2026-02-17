86d0a9ba10649e309931e5b111849e0d78851bad
Ben-DataPaws <167247129+Ben-DataPaws@users.noreply.github.com>
2026-02-17 13:23:54 -0500

    Create sp_MonitorAvailabilityGroup.sql

---
+++ sp_MonitorAvailabilityGroup.sql
@@ -0,0 +1,12 @@
+CREATE OR ALTER PROCEDURE [dbo].[sp_MonitorAvailabilityGroup]
+(
+	@CheckConfiguration BIT = 0,
+	@DefaultAlerting BIT = 1,
+	@Debug BIT = 0
+)
+AS
+BEGIN
+/*
+sp_MonitorAvailabilityGroup By DataPaws
+*/
+END
